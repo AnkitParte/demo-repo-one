03d5705a7ad8cb064bb63393ae3e85d95faf223e
Ankit parte <95030824+AnkitParte@users.noreply.github.com>
2022-07-19 15:55:53 +0530

    just clearing dots

--- Login.jsx
+++ Login.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AppContext } from "../AppContext";
+import { AppContext } from "./AppContext";
 
 export default function Login(){
     const [state,setState] = useState({email:"",password:""})
